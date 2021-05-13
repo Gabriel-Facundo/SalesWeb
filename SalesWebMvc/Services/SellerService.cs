55705d715960c597b5b1f28e729b2bdf8dcf59f3
Gabriel Facundo <gfacundo@outlook.com>
2021-05-13 19:39:22 -0300

    Created method to delete an Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-
+         
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
@@ -23,6 +23,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+        
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
 
